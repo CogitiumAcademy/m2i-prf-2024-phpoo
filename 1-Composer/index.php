1d53612ff1a81bf692011f94ed7daf40e197a0fa
PhG <info@cogitium.com>
2024-05-02 12:22:00 +0200

    Ajout du fichier index.php

---
+++ 1-Composer/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test de l'autoloading PSR-4</title>
+</head>
+
+<body>
+
+    <h1>Page de test</h1>
+
+
+</body>
+</html>
